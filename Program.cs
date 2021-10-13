ae18b365070962b768f6f42d93cfe2bb5c6d8e4a
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-10-13 18:17:01 -0300

    Se muestran todos los documentos de la colección. Se actualiza y elimina un documento

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Driver;
 using MongoDBInCSharp.Models;
 using System;
+using System.Collections.Generic;
 
 namespace MongoDBInCSharp
 {
@@ -12,10 +13,26 @@ namespace MongoDBInCSharp
             var database = client.GetDatabase("mdb-omg");
 
             var productsDB = database.GetCollection<Product>("products");
-            var product = new Product() { Name = "Alfajor", Price = 120, UnitOfMeasure = "gramos", Weigth = 80 };
-            productsDB.InsertOne(product);
 
-            Console.WriteLine("Todo OK");
+            //INSERT
+            //var product = new Product() { Name = "Alfombra", Price = 2000, UnitOfMeasure = "gramos", Weigth = 1000 };
+            //productsDB.InsertOne(product);
+
+            //GET
+            List<Product> products = productsDB.Find(f => true).ToList();
+            foreach (Product p in products)
+            {
+                Console.WriteLine("Name: " + p.Name + " Price: $" + p.Price);
+            }
+
+            //UPDATE
+            //var product = new Product() { Id= "61674644ad0773e4d171083e", Name = "Alfombra editada", Price = 2000, UnitOfMeasure = "kilos", Weigth = 1000 };
+            //productsDB.ReplaceOne(r => r.Id == "61674644ad0773e4d171083e", product);
+
+            //DELETE
+            productsDB.DeleteOne(d => d.Id == "61674644ad0773e4d171083e");
+
+
         }
     }
 }
