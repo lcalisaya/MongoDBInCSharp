1397b8ef102a15185c2c7af68cbcab25ccc83d25
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-10-13 17:22:53 -0300

    Se crea una app consola y se instala MongoDB.Driver

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoDBInCSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba con MongoDB y C#");
+        }
+    }
+}
