2188b52bb15e26b55fffa13ae6d02c715d1e21b9
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-10-13 17:40:23 -0300

    Creo un modelo especificandole a MongoDB el campo llave

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBInCSharp.Models
+{
+    public class Product
+    {
+        [BsonId] //Key field
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("price")]
+        public int Price { get; set; }
+        [BsonElement("weigth")]
+        public int Weigth { get; set; }
+        [BsonElement("unitOfMeasure")]
+        public string UnitOfMeasure { get; set; }
+    }
+}
